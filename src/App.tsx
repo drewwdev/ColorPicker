ede947b6c78b5996c7035b0e633a3bc05fd682a7
Drew Wilson <jankyy93@gmail.com>
2021-09-08 16:08:30 -0400

    html and css finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
 import React from 'react'
 export function App() {
-  return <div>Hello, World</div>
+  return (
+    <div>
+      <main>
+        <aside>
+          Color
+          <div className="colorpreview"></div>
+        </aside>
+        <div className="sliders">
+          <div className="hue">
+            H <input type="range" />
+          </div>
+          <div className="saturation">
+            S <input type="range" />
+          </div>
+          <div className="lightness">
+            L
+            <input type="range" />
+          </div>
+          <div className="alpha">
+            A
+            <input type="range" />
+          </div>
+        </div>
+      </main>
+    </div>
+  )
 }
