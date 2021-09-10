12671da23791f1b9f733718880336e2ee5d360fc
Drew Wilson <jankyy93@gmail.com>
2021-09-10 10:19:15 -0400

    Background color of element is changing when sliders change. Deleted components that do not need to be used

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,84 @@
-import React from 'react'
-import { Hue } from './components/hue'
-import { Saturation } from './components/saturation'
-import { Lightness } from './components/lightness'
+import React, { useState } from 'react'
 
 export function App() {
+  const [hue, setHue] = useState(180)
+  const [saturation, setSaturation] = useState(50)
+  const [lightness, setLightness] = useState(50)
+
+  const newBackgroundColor = `hsl(${hue}, ${saturation}%, ${lightness}%)`
+  const newStyle = { backgroundColor: newBackgroundColor }
+
+  function _hueSlider(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault()
+
+    const sliderHue = event.target.value
+
+    setHue(Number(sliderHue))
+  }
+  function _lightnessSlider(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault()
+
+    const sliderLightness = event.target.value
+
+    setLightness(Number(sliderLightness))
+  }
+  function _saturationSlider(event: React.ChangeEvent<HTMLInputElement>) {
+    event.preventDefault()
+
+    const sliderSaturation = event.target.value
+
+    setSaturation(Number(sliderSaturation))
+  }
+
   return (
     <div>
       <main>
         <aside>
           Color
-          <div className="colorpreview"></div>
+          <div className="colorpreview" style={newStyle}></div>
         </aside>
         <div className="sliders">
           <div>
-            H <Hue />
+            H
+            <div>
+              <input
+                type="range"
+                min="0"
+                max="360"
+                step="1"
+                value={hue}
+                onInput={_hueSlider}
+              />
+              {hue}
+            </div>
           </div>
           <div>
-            S <Saturation />
+            S
+            <div>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                step="1"
+                value={lightness}
+                onInput={_lightnessSlider}
+              />
+              {lightness}%
+            </div>
           </div>
           <div>
-            L <Lightness />
+            L
+            <div>
+              <input
+                type="range"
+                min="0"
+                max="100"
+                step="1"
+                value={saturation}
+                onInput={_saturationSlider}
+              />
+              {saturation}%
+            </div>
           </div>
         </div>
       </main>
